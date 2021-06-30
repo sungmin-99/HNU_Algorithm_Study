973624c825284a6be90c044a4cf10eb85039ab94
cute700 <cute_700@naver.com>
2021-06-30 14:38:56 +0900

    1110 - 완료 (문자열로도 풀어보기)

---
+++ 2021_UCPC_practice/practice_1110/practice_1110.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, left, right, addedRight;
+	int count = 0;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	int tmp = n;
+
+	while (1)
+	{
+		left = n / 10;
+		right = n % 10;
+		addedRight = (left + right) % 10;
+
+		n = (right * 10) + addedRight;
+
+		count++;
+		if (tmp == n)
+			break;
+	}
+	cout << count;
+
+	return 0;
+}
