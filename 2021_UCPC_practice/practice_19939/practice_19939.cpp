67d48cdef19458c56c5d62a3112c57c03fa2c516
DESKTOP-LAB <cute_700@naver.com>
2021-07-01 16:12:30 +0900

    1439 - 완료 19939 - 시작

---
+++ 2021_UCPC_practice/practice_19939/practice_19939.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// ¹Ú ÅÍ¶ß¸®±â
+
+int main()
+{
+	int n, k;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> k;
+
+	//if (n / 2 < k)
+	//	cout << -1;
+
+	cout << n / 2;
+	
+
+	return 0;
+}
