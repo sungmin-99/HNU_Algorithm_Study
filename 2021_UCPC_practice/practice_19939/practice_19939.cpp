89c9b21d813030b6419c511535535a2203bdad9f
DESKTOP-LAB <cute_700@naver.com>
2021-07-02 17:44:04 +0900

    19939 - 수정 중

--- 2021_UCPC_practice/practice_19939/practice_19939.cpp
+++ 2021_UCPC_practice/practice_19939/practice_19939.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 // 박 터뜨리기
 
 int main()
 {
-	int n, k;
+	int n, k, sum = 0;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	cin >> n >> k;
 
-	//if (n / 2 < k)
-	//	cout << -1;
+	if (n > 2 && k == 2)
+	{                    // 위 조건에서 n이
+		if (n % 2 == 0)  // 짝수면 무조건 2
+			cout << 2;
+		else             // 홀수면 무조건 1
+			cout << 1;
+		return 0;
+	}
 
-	cout << n / 2;
-	
+	for (int i = 1; i <= k; i++)
+		sum += i;
+
+	n -= sum;
+
+
 
 	return 0;
 }
