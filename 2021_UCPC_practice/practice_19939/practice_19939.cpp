5bb5785828a4c62fc3646361916aade3b3730d5d
snowman_desktop <cute_700@naver.com>
2021-07-03 21:38:52 +0900

    19939 - 맞음

--- 2021_UCPC_practice/practice_19939/practice_19939.cpp
+++ 2021_UCPC_practice/practice_19939/practice_19939.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 // ¹Ú ÅÍ¶ß¸®±â
@@ -9,15 +7,18 @@ int main()
 {
 	int n, k;
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> k;
 
-	//if (n / 2 < k)
-	//	cout << -1;
+	int min = (k + 1) * k / 2;  
 
-	cout << n / 2;
-	
+	if (n < min)
+		cout << -1;
+	else if ((n - min) % k == 0)
+		cout << k - 1;
+	else
+		cout << k;
 
 	return 0;
 }
