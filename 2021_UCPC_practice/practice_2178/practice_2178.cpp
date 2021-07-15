8c6c1a03d8628aaaa2867b2ba94fc2c3d96c469b
DESKTOP-LAB <cute_700@naver.com>
2021-07-15 11:06:19 +0900

    2178 - 노트북으로  작업하기 윈한 커밋

--- 2021_UCPC_practice/practice_2178/practice_2178.cpp
+++ 2021_UCPC_practice/practice_2178/practice_2178.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+
+int dx[] = { 0, -1, 0, 1 };
+int dy[] = { -1, 0, 1, 0 };
+int map[101][101];
 
 int main()
 {
+	int n, m;
+	cin >> n >> m;
 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			scanf_s("%1d", map[i][j]);
+	}
 }
