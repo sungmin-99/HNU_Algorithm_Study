8867e8649d9499cd11375c9393f24d9d322056bd
DESKTOP-LAB <cute_700@naver.com>
2021-08-03 14:55:55 +0900

    10026 - v3

--- cute700/4week/boj_10026/boj_10026.cpp
+++ cute700/4week/boj_10026/boj_10026.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <vector>
+#include <string.h>
 
 using namespace std;
 // 적록색약
 
 int n;
-char arr[100][100];
-bool visited[100][100];
+char arr[101][101];
+bool visited[101][101];
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 
@@ -14,21 +14,18 @@ void dfs(int x, int y)
 {
 
 	visited[x][y] = true;
+	if (x < 0 || x > 100 || y < 0 || y > 100)
+		exit(0);
 	for (int i = 0; i < 4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny >= 0 && ny <= 100 && nx >= 0 && nx <= 100)
+		if (!(visited[nx][ny]) && (arr[x][y] == arr[nx][ny])) // 방문 안했으면서 먼저 값과 값이 같다면 dfs
 		{
-			if (!(visited[nx][ny]) && (arr[x][y] == arr[nx][ny]))
-			{
-				dfs(nx, ny);
-			}
+			dfs(nx, ny);
 		}
 	}
-
 }
-
 
 int main()
 {
@@ -55,7 +52,7 @@ int main()
 			if (!visited[i][j])
 			{
 				dfs(i, j);
-				resultNormal += 1;
+				resultNormal++;
 			}
 		}
 	}
@@ -82,6 +79,6 @@ int main()
 		}
 	}
 
-	cout << resultNormal << ' ' << resultRG;
-
+	cout << resultNormal << ' ' << resultRG << endl;
+	return 0;
 }
